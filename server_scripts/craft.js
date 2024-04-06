3bd9087e7bc68c413d0dad806ab128adea3460b9
Leroy <TheonlyTazz@gmail.com>
2024-04-06 23:20:34 +0200

    Update craft.js
    Update to KJS6.0+

--- server_scripts/craft.js
+++ server_scripts/craft.js
@@ -1,7 +1,7 @@
 /**
  ** craft.js -- a craft.zs equivalent?
  *! Created by Theonlytazz
- * Date - 08.07.2023
+ * Date - 06.04.2024
  *? Thanks to Lexxie for her amazing utils <3
  *? Thanks to Daddy Aurug for all their help
  *? Thanks to many many more <3
@@ -11,7 +11,7 @@
  * (ex 12 triggerItems for a Diamond Chest)
  */
 
-onEvent('block.right_click', event =>{
+BlockEvents.rightClicked(event => {
 	const { block,item,player,level,server,target,entity, hand } = event
 	if(hand != 'MAIN_HAND') return
 	if(!player.isCrouching()) return
@@ -23,29 +23,27 @@ onEvent('block.right_click', event =>{
 	const allRemoval = 'minecraft:lava_bucket'
 	//------------------------SETTINGS----------------------------
 	
-
 	// If Container smaller than Vanilla Chest, return
-	let totalsize = block.inventory?.width ?? 0
+	let totalsize = block.inventory?.width * block.inventory?.height ?? 0
+
 	if (item != triggerItem ||
 		totalsize < 27) return
 
-
+    
 	let keys = 'ABCDEFGHI'
 	let inv = block.inventory
-	let width
 	let spacingW 
 	let spacingH
 
 	// Variable Width, useable if more than 9 slots per row
-	if (item.count > 1 && totalsize != [27 || 54]) {width = item.count}
-	else {width = 9}
-	let height = block.inventory.width / width
+	let width = block.inventory.width
+    let height = block.inventory.height
 	
 	let maxOffsetX = Math.floor(width / 4) 
 	let maxOffsetY = Math.floor(height / 3) 
 	let allRecipes = []
-	
-	player.tell(`\n-------------${Item.of(block.id).name.getString()}-------------`)
+
+	player.tell(`\n-------------${Item.of(block.id)}-------------`)
 
 	//Recipe Handler
 	for(let offsetY = 0; offsetY < maxOffsetY; offsetY++){
@@ -54,15 +52,16 @@ onEvent('block.right_click', event =>{
 			let index = offsetY*2+offsetY+offsetX+1
 
 			// Skip if no Output Item
-			let output = inv.get(offset+3 + width)
-			if(output.toString() == 'Item.empty') continue
-
+			let output = inv.getStackInSlot(offset+3 + width)
+			if(output.id == 'minecraft:air') continue
+            if(output.count > 1) output = `'${output.count}x ${output.id}'`
+            else output = `'${output.id}'`
 
 			// Raw Crafting Grid Map
 			let mapRaw = {
-				1: inv.get(offset+0),2: inv.get(offset+1),3: inv.get(offset+2),
-				4: inv.get(offset+0 + width),5: inv.get(offset+1 + width),6: inv.get(offset+2 + width),
-				7: inv.get(offset+0 + (width*2)),8: inv.get(offset+1 + (width*2)),9: inv.get(offset+2 + (width*2))
+				1: inv.getStackInSlot(offset+0),2: inv.getStackInSlot(offset+1),3: inv.getStackInSlot(offset+2),
+				4: inv.getStackInSlot(offset+0 + width),5: inv.getStackInSlot(offset+1 + width),6: inv.getStackInSlot(offset+2 + width),
+				7: inv.getStackInSlot(offset+0 + (width*2)),8: inv.getStackInSlot(offset+1 + (width*2)),9: inv.getStackInSlot(offset+2 + (width*2))
 			}
 
 
@@ -73,13 +72,14 @@ onEvent('block.right_click', event =>{
 			let shapelessList = []
 			for (let i = 1; i <= Object.values(mapRaw).length; i++) {
 				let item = mapRaw[i]
-				if(item.toString() != 'Item.empty') shapelessList.push(item.toString())
-				if(item.toString() != 'Item.empty' && !itemList.includes(item.toString())){
-					itemList.push(item.toString())
-					keyMap.push(`${keys[Object.values(keyMap).length]}: ${item.toString()}`)
+                
+				if(item.id != 'minecraft:air') shapelessList.push(`'${item.id}'`)
+				if(item.id != 'minecraft:air' && !itemList.includes(item.id)){
+					itemList.push(item.id)
+					keyMap.push(`${keys[Object.values(keyMap).length]}: '${item.id}'`)
 				}
-				if(item.toString() == 'Item.empty') gridList.push(' ')
-				else gridList.push(keys[itemList.indexOf(item.toString())])
+				if(item.id == 'minecraft:air') gridList.push(' ')
+				else gridList.push(keys[itemList.indexOf(item.id)])
 			}
 			let input = gridList.join('').match(/.{1,3}/g).toString().split(',').join(`','`) ?? []
 
@@ -87,11 +87,11 @@ onEvent('block.right_click', event =>{
 			// Removing Recipe Handler
 			let removeStr
 			let type
-			let removalItem = inv.get(offset+3 + (width*2)) ?? ''
-			let modifier = inv.get(offset+3)
+			let removalItem = inv.getStackInSlot(offset+3 + (width*2)) ?? ''
+			let modifier = inv.getStackInSlot(offset+3)
 			switch(removalItem){
 				case typeRemoval:{
-					switch(modifier.toString()){
+					switch(modifier.id){
 						case 'minecraft:stonecutter': {type = 'minecraft:stonecutter';break}
 						case 'minecraft:campfire': {type = 'minecraft:campfire_cooking';break}
 						case 'minecraft:smoker': {type = 'minecraft:smoking';break}
@@ -111,11 +111,11 @@ onEvent('block.right_click', event =>{
 			let recipe
 			type = "Shaped"
 			switch(modifier){
-				case 'minecraft:stonecutter': {recipe = `event.stonecutting(${output}, ${mapRaw[5]})`; type='Stonecutting'; break}
-				case 'minecraft:campfire': {recipe = `event.campfireCooking(${output}, ${mapRaw[5]})`; type='Campfire'; break}
-				case 'minecraft:smoker': {recipe = `event.smoking(${output}, ${mapRaw[5]})`; type='Smoking'; break}
-				case 'minecraft:blast_furnace': {recipe = `event.blasting(${output}, ${mapRaw[5]})`; type = 'Blasting'; break}
-				case 'minecraft:furnace': {recipe = `event.smelting(${output}, ${mapRaw[5]})`; type = 'Smelting';break}
+				case 'minecraft:stonecutter': {recipe = `event.stonecutting(${output}, '${mapRaw[5].id}')`; type='Stonecutting'; break}
+				case 'minecraft:campfire': {recipe = `event.campfireCooking(${output}, '${mapRaw[5].id}')`; type='Campfire'; break}
+				case 'minecraft:smoker': {recipe = `event.smoking(${output}, '${mapRaw[5].id}')`; type='Smoking'; break}
+				case 'minecraft:blast_furnace': {recipe = `event.blasting(${output}, '${mapRaw[5].id}')`; type = 'Blasting'; break}
+				case 'minecraft:furnace': {recipe = `event.smelting(${output}, '${mapRaw[5].id}')`; type = 'Smelting';break}
 				case 'minecraft:glass_pane':{recipe = `event.shapeless(${output}, [${shapelessList.toString()}])`; type = 'Shapeless'; break}
 				default:{recipe = `event.shaped(${output}, ['${input}'], {${keyMap.toString()}})`}
 			}
@@ -125,7 +125,7 @@ onEvent('block.right_click', event =>{
 			// Printing Lines
 
 			// Removal Recipe String Print
-			if(removalItem.toString() != 'Item.empty'){
+			if(removalItem.id != 'minecraft:air'){
 				let removeString
 				if (removalItem == allRemoval) removeString = buildString(index, output, recipe, 'All', true)
 				else if (removalItem == typeRemoval) removeString = buildString(index, output, recipe, type, true)
@@ -140,8 +140,8 @@ onEvent('block.right_click', event =>{
 	}
 	// All Recipe String Print
 	if(allRecipes.length <= 1) return
-	player.tell(Component.join(' ', [Component.darkRed(`\n[A]`).click(`copy:${allRecipes.join('\n')}`).hover(`All Recipes`), 
-									Component.white(`- all Recipes`)]))
+	player.tell([' ', [Component.darkRed(`\n[A]`).click(`copy:${allRecipes.join('\n')}`).hover(`All Recipes`), 
+									Component.white(`- all Recipes`)]])
 })
 
 
@@ -158,15 +158,15 @@ onEvent('block.right_click', event =>{
 function buildString(index, output, recipe, type, removal){
 	removal = removal ?? false
 	
-	let itemName = Item.of(output).name.getString()
+	let itemName = output
 	let clickEvent = 'copy:' + recipe
 	let hoverText
 	let recipeInfo
 	if(removal){
-		hoverText = Component.join('', [Component.white(`Removal Recipe of: \n    `),Component.white(`${Item.of(output).name.getString()}`),Component.white(`\n    Type: ${type.toString()}`)])
+		hoverText = ['', [Component.white(`Removal Recipe of: \n    `),Component.white(`${Item.of(output)}`),Component.white(`\n    Type: ${type.toString()}`)]]
 		recipeInfo = Component.white(` - Removal Recipe: `).append(itemName)
 	} else {
-		hoverText = Component.join(' ', [Component.white(`${type}`), Component.white('Recipe of:\n    '), Component.white(`${itemName}`)])
+		hoverText = [' ', [Component.white(`${type}`), Component.white('Recipe of:\n    '), Component.white(`${itemName}`)]]
 		recipeInfo = Component.white(` - ${type} Recipe: `).append(itemName)
 	}
 
@@ -175,13 +175,3 @@ function buildString(index, output, recipe, type, removal){
 	return textOutput
 
 }
-
-onEvent('recipes', event =>{
-
-	event.remove({output: 'minecraft:cut_copper'})
-	event.shapeless('minecraft:cut_copper', ['minecraft:iron_ingot','minecraft:nether_star','minecraft:oak_planks','minecraft:nether_star','minecraft:iron_block','minecraft:nether_star','minecraft:birch_log','minecraft:nether_star','minecraft:oak_log'])
-	event.smelting('minecraft:chest', 'minecraft:oak_log')
-	event.blasting('minecraft:oak_log', 'minecraft:iron_ingot')
-	event.remove({type: 'Blasting', output: '4x minecraft:chest'})
-	event.shaped('4x minecraft:chest', ['A A','A A','AAA'], {A: 'minecraft:oak_log'})
-})
